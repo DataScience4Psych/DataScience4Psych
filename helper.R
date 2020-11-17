08305c079ae5d16ca6088a7382798f17af7a0e32
Mason Garrison <s.mason.garrison@gmail.com>
2020-11-16 19:21:21 -0500

    tweeking

---
+++ helper.R
@@ -0,0 +1,5 @@
+install.packages(c("bookdown", "devtools", "dichromat", "DT", "fs", "gapminder", "gender", "geonames", "git2r", "glue", "gridExtra", "htmltools", "httr", "knitr", "RColorBrewer", "rebird", "rmarkdown", "rplos", "rvest", "testthat", "tidyverse", "usethis", "viridis", "xfun", "xml2", "ropensci/genderdata", "rstudio/gt"))
+install.packages("gt")
+install.packages(c("tidyverse", "discord"))
+install.packages("osfr")
+install.packages("genderdata", repos = "https://dev.ropensci.org", type = "source")
