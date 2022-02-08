358497df82bc80c0f47977dd8ac7b4915b4deb58
Mason Garrison <garrissm@wfu.edu>
2022-02-08 10:24:56 -0500

    Create packages.R

---
+++ packages.R
@@ -0,0 +1,19 @@
+#libraries
+install.packages("devtools")
+install.packages(c("tidyverse", "rmarkdown"))
+install.packages(c("bookdown", "discord"))
+install.packages("vembedr")
+devtools::install_github("gadenbuie/tweetrmd")
+install.packages("datasauRus")
+install.packages("DT")
+install.packages("car")
+install.packages("latticeExtra")
+install.packages("manipulateWidget")
+install.packages("shiny")
+install.packages("rgl")
+devtools::install_github('gmonette/spida2')
+install.packages("p3d", repos = "http://R-Forge.R-project.org")
+
+install.packages("tufte")
+devtools::install_github("rstudio-education/dsbox")
+
