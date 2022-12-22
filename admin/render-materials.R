dbffea93800cf3b77a04b30624bca14926f6c7af
Mason Garrison <garrissm@wfu.edu>
2022-12-22 13:54:16 -0500

    push

--- admin/render-materials.R
+++ admin/render-materials.R
@@ -5,26 +5,33 @@ library(pacman)
 pacman::p_load(coronavirus,
                devtools,
                discord,
+               datasauRus,
                fivethirtyeight,
                fs,
+               gapminder,
                ggbeeswarm,
                ggrepel,
                ggridges,
                here,
                hexbin,
+               gt,
  #              maps,
                NlsyLinks,
                patchwork,
                rmarkdown,
-               skimr,
+               skimr,dsbox,
+ tufte,
                tidyverse,
                uuid,
 #               vroom,
-               xaringan
+               xaringan,
+webshot,
+manipulateWidget
               )
 
-
-
+if (!require("spida2"))  devtools::install_github('gmonette/spida2')
+if (!require("p3d"))  install.packages("p3d", repos = "http://R-Forge.R-project.org")
+devtools::install_github("rstudio-education/dsbox")
 # xaringan ---------------------------------------------------------------------
 
 xaringans <- dir_info(recurse = 3, glob = "*.Rmd") %>%
