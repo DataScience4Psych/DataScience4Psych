3d493df6f6fdefd655a4b169cb63a29217a42e1f
Mason Garrison <garrissm@wfu.edu>
2022-12-27 08:51:33 -0500

    push

--- admin/render-materials.R
+++ admin/render-materials.R
@@ -2,42 +2,71 @@
 
 if (!require("pacman")) install.packages("pacman")
 library(pacman)
-pacman::p_load(coronavirus,
-               devtools,
-               discord,
+pacman::p_load(BiocManager,
+               bookdown,
+               coronavirus,
                datasauRus,
+               devtools,
+               dichromat,
+               discord,
+               dsbox,
+               DT,
                fivethirtyeight,
                fs,
                gapminder,
+               gender,
+               geonames,
                ggbeeswarm,
                ggrepel,
                ggridges,
+               git2r,
+               glue,
+               gridExtra,
+               gt,
                here,
                hexbin,
-               gt,maps,
-               NlsyLinks,
-               patchwork,
-               rmarkdown,
-               skimr,dsbox,
-               tufte,
-               tidyverse,
-               uuid,
-               vroom,
-               xaringan,
-               BiocManager,
-               webshot,
+               htmltools,
+               httr,
+               knitr,
                manipulateWidget,
-               rafalib
-              )
+               maps,
+               mosaicData,
+               NlsyLinks,
+               osfr,
+               openintro,
+               patchwork,
+               performance,
+               rafalib,
+               RColorBrewer,
+               rebird,
+               rmarkdown,
+               rplos,
+               rvest,
+               skimr,
+               see,
+               testthat,
+               tidyverse,
+               tufte,
+               usethis,
+               uuid,
+               viridis,
+               vroom,
+               webshot,
+               xaringan,
+               xfun,
+               xml2
+)
 
-if (!require("genderdata")) install.packages("genderdata", repos = "https://dev.ropensci.org", type = "source")
 
+if (!require("emo")) devtools::install_github("hadley/emo")
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 if (!require("Biobase")) BiocManager::install("Biobase")
-if (!require("SpikeInSubset"))BiocManager::install("SpikeInSubset")
+if (!require("SpikeInSubset")) BiocManager::install("SpikeInSubset")
 if (!require("spida2"))  devtools::install_github('gmonette/spida2')
 if (!require("p3d"))  install.packages("p3d", repos = "http://R-Forge.R-project.org")
-devtools::install_github("rstudio-education/dsbox")
+if (!require("dsbox")) devtools::install_github("rstudio-education/dsbox")
+# if (!require("genderdata")) install.packages("genderdata", repos = "https://dev.ropensci.org", type = "source")
+
 # xaringan ---------------------------------------------------------------------
 
 xaringans <- dir_info(recurse = 3, glob = "*.Rmd") %>%
@@ -46,8 +75,3 @@ xaringans <- dir_info(recurse = 3, glob = "*.Rmd") %>%
   pull(path)
 walk(xaringans, render)
 
-install.packages(c("bookdown", "devtools", "dichromat", "DT", "fs", "gapminder", "gender", "geonames", "git2r", "glue", "gridExtra", "htmltools", "httr", "knitr", "RColorBrewer", "rebird", "rmarkdown", "rplos", "rvest", "testthat", "tidyverse", "usethis", "viridis", "xfun", "xml2", "ropensci/genderdata", "rstudio/gt"))
-install.packages("gt")
-install.packages(c("tidyverse", "discord"))
-install.packages("osfr")
-install.packages("genderdata", repos = "https://dev.ropensci.org", type = "source")
