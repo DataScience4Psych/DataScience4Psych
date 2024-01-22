090fdfb40f4dea4502fb8fadf8415ee37e3f1043
Mason Garrison <garrissm@wfu.edu>
2024-01-22 01:03:35 -0500

    Update common.R
    cleaned up common.R and reran packages

--- admin/packages.R
+++ admin/packages.R
@@ -1,38 +1,76 @@
-# libraries
+# Conditional Libraries Installation
 
-install.packages(c(
-  "devtools",
-  "tidyverse",
-  "rmarkdown",
-  "bookdown",
-  "discord",
-  "vembedr",
-  "datasauRus",
-  "DT", "gt",
-  "car",
-  "latticeExtra",
-  "manipulateWidget",
-  "shiny",
-  "rgl", "BGmisc", "leaflet", "mosaicData",
-  "performance", "see", "plotly", "RefManageR"
-))
-devtools::install_github("gadenbuie/tweetrmd")
-devtools::install_github("gmonette/spida2")
-install.packages("p3d", repos = "http://R-Forge.R-project.org")
-if (!require("gapminder")) install.packages("gapminder")
+# Function to conditionally install a package
+install_if_missing <- function(package) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package)
+  }
+}
 
-install.packages("tufte")
-devtools::install_github("rstudio-education/dsbox")
+# Function to conditionally install a package from GitHub
+install_github_if_missing <- function(repo) {
+  package <- unlist(strsplit(repo, "/"))[2]
+  if (!require(package, character.only = TRUE)) {
+    devtools::install_github(repo)
+  }
+}
 
-install.packages("rafalib")
-devtools::install_github("crsh/citr")
-if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-BiocManager::install("Biobase")
-BiocManager::install("SpikeInSubset")
-install.packages("pacman")
-devtools::install_github("hadley/emo")
-install.packages("openintro")
-install.packages("ggridges")
-install.packages(c("robotstxt", "tidymodels"))
-devtools::install_github("gmonette/spida2")
-install.packages("p3d", repos = "http://R-Forge.R-project.org")
+# Function to conditionally install a package from R-Forge
+install_rforge_if_missing <- function(package, repo = "http://R-Forge.R-project.org") {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, repos = repo)
+  }
+}
+
+# Function to conditionally install a package from Bioconductor
+install_bioc_if_missing <- function(package) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    BiocManager::install(package)
+  }
+}
+
+
+
+# CRAN Packages
+cran_packages <- c(
+  "devtools", "tidyverse", "rmarkdown", "bookdown", "discord",
+  "vembedr", "datasauRus", "DT", "gt", "car", "latticeExtra",
+  "manipulateWidget", "shiny", "rgl", "BGmisc", "leaflet",
+  "mosaicData", "performance", "see", "plotly", "RefManageR",
+  "tufte", "rafalib", "openintro", "ggridges", "robotstxt",
+  "tidymodels", "pacman", "gapminder"
+)
+
+# Install CRAN packages if missing
+lapply(cran_packages, install_if_missing)
+
+# GitHub Packages
+github_packages <- c(
+  "gadenbuie/tweetrmd",
+  "gmonette/spida2",
+  "rstudio-education/dsbox",
+  "crsh/citr",
+  "hadley/emo"
+)
+
+# Install GitHub packages if missing
+lapply(github_packages, install_github_if_missing)
+
+
+# R-Forge Packages
+r_forge_packages <- c("p3d")
+
+lapply(r_forge_packages, install_rforge_if_missing)
+
+
+
+# Bioconductor Packages
+bioc_packages <- c("Biobase", "SpikeInSubset")
+
+# Ensure BiocManager is installed
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+
+# Install Bioconductor packages if missing
+lapply(bioc_packages, install_bioc_if_missing)
--- common.R
+++ common.R
@@ -1,79 +1,51 @@
-## seeded with content from same in adv-r
-## deleted bits that seem irrelevant
-## commented out bits that look like they may become relevant
-
-
-
 ## Standard Libraries
-
-library(tweetrmd) # ... embedding tweets
-library(vembedr) # ... embedding youtube videos
+library(tweetrmd)     # Embedding tweets
+library(vembedr)      # Embedding YouTube videos
 library(knitr)
 library(webshot)
 library(tidyverse)
 library(htmlwidgets)
 
-
-## Options
+## Options for knitr chunks
 knitr::opts_chunk$set(
   comment = "#>",
   error = TRUE,
   collapse = TRUE
-  # cache = TRUE,
-  # fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
-  # dpi = 300,
-  # out.width = "70%",
-  # fig.align = 'center',
-  # fig.width = 6,
-  # fig.asp = 0.618,  # 1 / phi
-  # fig.show = "hold"
-)
-options(
-  rlang_trace_top_env = rlang::current_env(),
-  rlang__backtrace_on_error = "none"
+  # Additional options can be included as needed
 )
 
+## Global options
 options(
+  rlang_trace_top_env = rlang::current_env(),
+  rlang__backtrace_on_error = "none",
   digits = 3,
   str = strOptions(strict.width = "cut")
 )
 
+## Additional options for LaTeX output
 if (knitr::is_latex_output()) {
   knitr::opts_chunk$set(width = 69)
-  options(width = 69)
-  options(crayon.enabled = FALSE)
-  options(cli.unicode = TRUE)
+  options(width = 69, crayon.enabled = FALSE, cli.unicode = TRUE)
 }
 
-# knitr::knit_hooks$set(
-#   small_mar = function(before, options, envir) {
-#     if (before) {
-#       par(mar = c(4.1, 4.1, 0.5, 0.5))
-#     }
-#   }
-# )
-
-
+## Knit hooks
 knitr::knit_hooks$set(chunk_envvar = function(before, options, envir) {
   envvar <- options$chunk_envvar
   if (before && !is.null(envvar)) {
     old_envvar <<- Sys.getenv(names(envvar), names = TRUE, unset = NA)
     do.call("Sys.setenv", as.list(envvar))
-    # print(str(options))
   } else {
     do.call("Sys.setenv", as.list(old_envvar))
   }
 })
 
-
-# datasets
-
-ds4p_funyoutube <- read.csv("admin/csv/ds4p_funyoutube.csv",
-  sep = ""
-)
+## Reading datasets
+ds4p_funyoutube <- read.csv("admin/csv/ds4p_funyoutube.csv", sep = "")
 ds4p_urls <- read.csv("./admin/csv/ds4p_urls.csv")
 
-# Functions
+## Functions
+
+# Check and install packages quietly
 check_quietly <- purrr::quietly(devtools::check)
 install_quietly <- purrr::quietly(devtools::install)
 
@@ -89,8 +61,7 @@ pretty_install <- function(...) {
   c(output, out$messages)
 }
 
-
-## no surprises
+# Function to handle single or multiple samples
 sample_no_surprises <- function(x) {
   if (length(x) <= 1) {
     return(x)
@@ -99,18 +70,14 @@ sample_no_surprises <- function(x) {
   }
 }
 
-# show slides better
-slide_url <- function(df_url,
-                      title,
-                      slide = NULL) {
+# Function to construct slide URLs
+slide_url <- function(df_url, title, slide = NULL) {
   var_url <- paste0(df_url$link[df_url$title == title], slide)
   return(var_url)
 }
 
-# try include_tweet
-try_include_tweet <- function(tweet_url,
-                              plain = FALSE, ...) {
+# Function to try including a tweet
+try_include_tweet <- function(tweet_url, plain = FALSE, ...) {
   return(try(tweetrmd::include_tweet(tweet_url = tweet_url, plain = plain, ...),
-    silent = TRUE
-  ))
+             silent = TRUE))
 }
