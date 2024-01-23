9c9bf737ecf5049fdad4bbee500af7b8077b97b8
Mason Garrison <garrissm@wfu.edu>
2024-01-23 11:31:02 -0500

    fixed :)

--- admin/fullindex.R
+++ admin/fullindex.R
@@ -1,31 +1,36 @@
 # Libraries
-library(gt)
 library(tidyverse)
 
 # Functions
 make_full <- function(myurl, gituser = "datascience4psych", myrepo = "DataScience4Psych") {
   paste0("https://", gituser, ".github.io/", myrepo, "/", myurl)
 }
+
 make_hyperlink <- function(myurl, mytext = myurl) {
   paste0('<a href="', myurl, '">', mytext, "</a>")
 }
 
-df_web <- list.files(path = "docs/", "[.]html$", recursive = TRUE)
+# File paths
+df_web <- list.files(path = "docs/", pattern = "\\.html$", recursive = TRUE)
 
-df_slides <- list.files(path = "E:\\Dropbox\\Lab\\Teaching\\DS4P\\ds4p-materials\\slides", "[.]html$", recursive = TRUE)
+# Exclude 'workshop' subfolder from slides
+df_slides <- list.files(path = "E:\\Dropbox\\Lab\\Teaching\\DS4P\\slides\\slides", pattern = "\\.html$", recursive = TRUE) %>%
+  grep(pattern = "workshop", x = ., value = TRUE, invert = TRUE)
 
+# Combine and label data
 df_docs <- c(df_web, df_slides)
+domain <- c(rep("DataScience4Psych", length(df_web)),
+            rep("slides", length(df_slides)))
 
-domain <- c(rep("DataScience4Psych", length(df_web)), rep("slides", length(df_slides)))
-
-df <- data.frame(
-  stringsAsFactors = FALSE,
-  title = str_replace(
-    str_remove_all(df_docs, "(u\\d_d\\d+|\\.html|[-])"), "/.*", ""
-  ),
+# Data frame creation with vectorized operations
+df <- tibble(
+  title = basename(df_docs) %>%
+    str_remove(".html") %>%
+    str_replace_all("[-.]", ""),
   link = make_full(df_docs, myrepo = domain)
-)
+) %>%
+  mutate(title = if_else(link == "https://datascience4psych.github.io/slides/d13_goodviz/d13b_moreggplot.html",
+                         "d13b_moreggplot", title))
 
-df$title[df$link == "https://datascience4psych.github.io/slides/d13_goodviz/d13b_moreggplot.html"] <- "d13b_moreggplot"
-
-write_csv(df, "data/ds4p_urls.csv")
+# Save to CSV
+write_csv(df, "admin/csv/ds4p_urls.csv")
