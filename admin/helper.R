7e272855d2b97c87dee49eb62594bb0b7fce992c
Mason Garrison <garrissm@wfu.edu>
2024-01-30 10:12:01 -0500

    cleaning up

--- admin/helper.R
+++
@@ -1,6 +0,0 @@
-install.packages(c("bookdown", "devtools", "dichromat", "DT", "fs", "gapminder", "gender", "geonames", "git2r", "glue", "gridExtra", "htmltools", "httr", "knitr", "RColorBrewer", "rebird", "rmarkdown", "rplos", "rvest", "testthat", "tidyverse", "usethis", "viridis", "xfun", "xml2", "ropensci/genderdata", "rstudio/gt"))
-install.packages("gt")
-install.packages(c("tidyverse", "discord"))
-install.packages("osfr")
-
-usethis::use_github_action(url = "https://raw.githubusercontent.com/ropenscilabs/actions_sandbox/master/.github/workflows/deploy_bookdown.yml")
