7fb8946cf955162cdb882bd7df76b4edc8c66459
Mason Garrison <garrissm@wfu.edu>
2026-01-13 11:23:10 -0500

    restore manual build

---
+++ docs/anchor-links.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // Section anchors
+  $('.section h1, .section h2, .section h3, .section h4, .section h5').each(function() {
+    anchor = '#' + $(this).parent().attr('id');
+    $(this).addClass("hasAnchor").prepend('<a href="' + anchor + '" class="anchor"></a>');
+  });
+});
