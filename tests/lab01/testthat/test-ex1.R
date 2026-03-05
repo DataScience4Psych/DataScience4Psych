5d024e2b376b39a09740f44d80cf4c44ade81afa
Mason Garrison <garrissm@wfu.edu>
2026-03-05 15:30:59 -0500

    Update test-ex1.R

--- tests/lab01/testthat/test-ex1.R
+++ tests/lab01/testthat/test-ex1.R
@@ -5,15 +5,25 @@ test_that("Ex 1: datasaurus_dozen has 1846 rows", {
 
   # check if 1846 is in the rmd file ".rmd_content"
 
-  an_in_rmd <- any(str_detect(.rmd_content, "1846")||str_detect(.rmd_content, "nrow(datasaurus_dozen)"))
+  potential_answers <- c("1846", "nrow\\(datasaurus_dozen\\)")
+  pattern <- paste0("(", paste(potential_answers, collapse = "|"), ")")
+
+
+  answer_in_rmd <- stringr::str_detect(.rmd_content, pattern) |> any()
 
   expect_true(an_in_rmd, info = "Make sure to include the number of rows in the .rmd file")
 
 })
 
 test_that("Ex 1: datasaurus_dozen has 3 columns", {
-  data("datasaurus_dozen")
-  expect_equal(ncol(datasaurus_dozen), 3)
+
+  potential_answers <- c("3\s?columns", "(ncol|length)\\(datasaurus_dozen\\)")
+  pattern <- paste0("(", paste(potential_answers, collapse = "|"), ")")
+
+
+  answer_in_rmd <- stringr::str_detect(.rmd_content, pattern) |> any()
+
+  expect_true(an_in_rmd, info = "Make sure to include the number of columns in the .rmd file")
 })
 
 test_that("Ex 1: datasaurus_dozen has variables dataset, x, and y", {
